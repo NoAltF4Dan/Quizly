fa9c4c1eae4dc12a29b5f7d4ef66f810938b09b1
dnwrs <dnwrs@gmail.com>
2025-10-09 12:26:22 +0900

    fix settings installed_apps

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'auth_app',
     'quiz_app',
 ]
